aa11a30fb09d16723fd56c9e37e99b1552c3d8da
samarth sable <samarthsable0005@gmail.com>
2025-02-09 12:25:15 +0530

    CRUD project completed

--- Myproject/School/urls.py
+++ Myproject/School/urls.py
@@ -5,4 +5,6 @@ from School import views
 
 urlpatterns = [
     path('',views.home, name='home'),
+    path('shoe/update/<int:shoe_id>/', views.update_shoe, name='update_shoe'),
+    path('shoe/delete/<int:shoe_id>/', views.delete_shoe, name='delete_shoe'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Myproject/School/views.py
+++ Myproject/School/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render, HttpResponseRedirect,get_object_or_404
 from .models import Shoes
 from .forms import ShoeForm
 
@@ -15,3 +15,23 @@ def home(request):
         form = ShoeForm()  # Initialize an empty form for GET request
 
     return render(request, 'school/home.html', {'form': form, 'shoes': shoes})  # Ensure 'shoes' matches template
+
+def update_shoe(request, shoe_id):
+    shoe = Shoes.objects.get(pk=shoe_id)
+    if request.method == "POST":
+        form = ShoeForm(request.POST, request.FILES, instance=shoe)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/")  # Redirect to shoe list or shoe details page
+    else:
+        form = ShoesForm(instance=shoe)
+
+    return render(request, 'update_shoe.html', {'form': form})
+
+def delete_shoe(request, shoe_id):
+    shoe = get_object_or_404(Shoes, id=shoe_id)
+    if request.method == "POST":
+        shoe.delete()
+        return HttpResponseRedirect("/")  # Redirect to shoe list after deletion
+
+    return render(request, 'confirm_delete.html', {'shoe': shoe})
